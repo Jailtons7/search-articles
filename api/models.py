bc03d59d75fb2827582ee1bbd01bc1d4851d3613
Jailton Silva <jss1@ifal.edu.br>
2021-12-18 11:55:50 -0300

    feat: adding TblOrgaoIeExerc model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class TblOrgaoIeExerc(models.Model):
+    codSiglaOrgaoIE = models.CharField("Código da IE", max_length=10, primary_key=True)
+    nomOrgaoIEExerc = models.CharField("Nome da IE", max_length=50)
+
+    class Meta:
+        verbose_name = "Instituição de Ensino de Exercício"
+        verbose_name_plural = "Instituições de Ensino de Exercício"
+
+    def __str__(self):
+        return f"{self.nomOrgaoIEExerc} - {self.codSiglaOrgaoIE}"
