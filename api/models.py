09d0ceefd4eac17ed568591acc64a5b471e5e866
Jailton Silva <jss1@ifal.edu.br>
2021-12-18 19:09:10 -0300

    feat: adding tblTitulosArtigos and tblArtigoPublicado

--- api/models.py
+++ api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+SIM_NAO_OPTIONS = (
+    ("SIM", "SIM"),
+    ("NAO", "NÂO"),
+)
+
+
 class TblOrgaoIeExerc(models.Model):
     codSiglaOrgaoIE = models.CharField("Código da IE", max_length=10, primary_key=True)
     nomOrgaoIEExerc = models.CharField("Nome da IE", max_length=50)
@@ -38,8 +44,51 @@ class TblServidores(models.Model):
         return self.NomServidor
 
 
+class TblIssn(models.Model):
+    CodIssn = models.AutoField(primary_key=True)
+    DscIssn = models.CharField(max_length=20)
+    DscIssnTituloJornal = models.CharField(max_length=195)
+
+    class Meta:
+        verbose_name = "ISSN"
+        verbose_name_plural = "ISSNs"
+
+    def __str__(self):
+        return self.DscIssn
+
+
+class TblTitulosArtigos(models.Model):
+    CodTitulosArtigos = models.AutoField(primary_key=True)
+    DscTitulosArtigos = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Titulo de Artigo"
+        verbose_name_plural = "Titulos de Artigos"
+
+    def __str__(self):
+        return self.DscTitulosArtigos
+
+
 class TblArtigoPublicado(models.Model):
-    pass
+    CodArtigoPublicado = models.AutoField(primary_key=True)
+    ART = models.CharField(max_length=255)
+    # ver o que significa o campo ID_ARTIGO na tabela original
+    TipArtigoRelevancia = models.CharField(max_length=3, choices=SIM_NAO_OPTIONS)
+    TipArtigoDivulgacaoCientifica = models.CharField(max_length=3, choices=SIM_NAO_OPTIONS)
+    CodSiglaAtuacaoProfissionalLattes = models.ForeignKey("TblOrgaoIeExerc", on_delete=models.PROTECT)
+    CodIssn = models.ForeignKey("TblIssn", on_delete=models.PROTECT)
+    CodTitulosArtigos = models.ForeignKey("TblTitulosArtigos", on_delete=models.PROTECT)
+    AnoPublicacaoArtigo = models.CharField(max_length=4)
+
+    class Meta:
+        verbose_name = "Artigo Publicado"
+        verbose_name_plural = "Artigos Publicados"
+
+    def __str__(self):
+        return self.get_art()
+
+    def get_art(self) -> str:
+        return self.ART[0:40] + "..." if len(self.ART) > 40 else self.ART
 
 
 class TblProjetos(models.Model):
@@ -82,5 +131,3 @@ class TblArtigoPublicadoPalavraChave(models.Model):
     pass
 
 
-class TblTitulosArtigos(models.Model):
-    pass
