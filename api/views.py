73b19129ecb92f57221cead5828214003daedaef
Jailton Silva <jss1@ifal.edu.br>
2022-01-06 11:40:36 -0300

    feat: adding view ArtigosPublicadosViews

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.models import TblArtigoPublicado
+from api.serializers import ArtigoPublicadoSerializer
+
+
+class ArtigosPublicadosViews(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ArtigoPublicadoSerializer
+    queryset = TblArtigoPublicado.objects.all()
+
+    def get_queryset(self):
+        query = self.queryset
+        filters = self.request.GET
+        ano = filters.get('ano')
+        if ano:
+            query = query.filter(AnoPublicacaoArtigo__exact=ano)
+        titulo = filters.get('titulo')
+        if titulo:
+            query = query.filter(CodTitulosArtigos__DscTitulosArtigos__icontains=titulo)
+        autor = filters.get('autor')
+        if autor:
+            query = query.filter(CodServidor__NomServidor__icontains=autor)
+        return query
