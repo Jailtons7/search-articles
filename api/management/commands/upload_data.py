d1bb44ba01c08800ae338d17f8bf2977bf2df154
Jailton Silva <jss1@ifal.edu.br>
2022-01-06 11:29:44 -0300

    feat: adding custom command to load data into models

--- api/management/commands/upload_data.py
+++ api/management/commands/upload_data.py
@@ -1,27 +1,20 @@
+import sys
 import json
 
+from django.apps import apps
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError, models
-
-from api.models import (
-    TblIssn,
-    TblTitulosArtigos,
-    TblOrgaoIeExerc,
-    TblCargos,
-    TblGrandeArea,
-    TblPalavraChaveArt,
-    TblArtigoPublicado,
-    TblServidores,
-    TblArea,
-    TblArtigoPublicadoPalavrasChave,
-    TblArtigoPublicadoAreaConhecimento,
-    TblSubArea,
-    TblEspecialidade,
-)
 
 
 class Command(BaseCommand):
+    help = """
+    Populates database passed in --model option.
+    
+    To each model you want to populate,
+    you should provide a json file in PROJECT_ROOT/api/fixtures/.
+    """
+
     @staticmethod
     def json_to_dict(file_name: str) -> dict:
         file_path = f'{str(settings.BASE_DIR)}/api/fixtures/{file_name}'
@@ -29,79 +22,53 @@ class Command(BaseCommand):
             dic = json.load(f)
             return dic
 
+    @staticmethod
+    def modify_data(d: dict, fks: list, not_used: list = []):
+        """
+        Avoid adding an object in foreign keys
+        """
+        for value in fks:
+            d[f'{value}_id'] = d[value]
+            del d[value]
+
+        for value in not_used:
+            del d[value]
+
     def insert_data_to_model(self, model: models.Model, dict_data: dict):
         try:
             model.objects.create(**dict_data)
         except IntegrityError:
-            self.stdout.write(f'{model._meta.object_name} was already populated')
+            self.stdout.write(f'{model._meta.object_name}: Integrity issue with data {dict_data}')
 
     def add_arguments(self, parser):
-        parser.add_argument('--model', action='append', type=str)
-        parser.add_argument('--foregeign_key', action='append', nargs='+', type=str)
+        parser.add_argument('-m', '--model', type=str)
+        parser.add_argument('-f', '--foregeign_key', action='append', nargs='+', type=str)
+        parser.add_argument('-n', '--not_use', action='append', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        self.stdout.write('Starting data upload')
-        dict_issn = self.json_to_dict('TblIssn.json')
-        for data in dict_issn:
-            self.insert_data_to_model(model=TblIssn, dict_data=data)
-        del dict_issn
+        model_nm = options.get('model')
+        list_fkeys = options.get('foregeign_key')
+        not_used_fields = options.get('not_use') or [[]]
 
-        article_titles = self.json_to_dict('TblTitulosArtigos.json')
-        for data in article_titles:
-            self.insert_data_to_model(model=TblTitulosArtigos, dict_data=data)
-        del article_titles
+        if model_nm is None:
+            raise CommandError("--model argument is required")
 
-        ie_exercicio = self.json_to_dict('TblOrgaoIeExerc.json')
-        for data in ie_exercicio:
-            self.insert_data_to_model(model=TblOrgaoIeExerc, dict_data=data)
-        del ie_exercicio
+        all_models = apps.all_models['api'].keys()
+        if model_nm.lower() not in all_models:
+            raise CommandError("Please, provide a valid model name.")
 
-        cargos = self.json_to_dict('TblCargos.json')
-        for data in cargos:
-            self.insert_data_to_model(model=TblCargos, dict_data=data)
-        del cargos
+        self.stdout.write(self.style.SUCCESS('Starting data upload'))
+        self.stdout.write(self.style.WARNING('It may take a couple of minutes.'))
+        try:
+            model = getattr(sys.modules['api.models'], model_nm)
+        except AttributeError:
+            raise CommandError(f"{model_nm} is not a valid model. Tip: The --model option is case sensitive.")
 
-        grande_area = self.json_to_dict('TblGrandeArea.json')
-        for data in grande_area:
-            self.insert_data_to_model(model=TblGrandeArea, dict_data=data)
-        del grande_area
+        data_madel = self.json_to_dict(f'{model.__name__}.json')
+        for data in data_madel:
+            if list_fkeys:
+                fkeys = list_fkeys[0]
+                self.modify_data(data, fkeys, not_used_fields[0])
+            self.insert_data_to_model(model=model, dict_data=data)
 
-        palavra_chave_artigo = self.json_to_dict('TblPalavraChaveArt.json')
-        for data in palavra_chave_artigo:
-            self.insert_data_to_model(model=TblPalavraChaveArt, dict_data=data)
-        del palavra_chave_artigo
-
-        artigo_publicado = self.json_to_dict('TblArtigoPublicado.json')
-        for data in artigo_publicado:
-            self.insert_data_to_model(model=TblArtigoPublicado, dict_data=data)
-        del artigo_publicado
-
-        servidores = self.json_to_dict('TblServidores.json')
-        for data in servidores:
-            self.insert_data_to_model(model=TblServidores, dict_data=data)
-        del servidores
-
-        area = self.json_to_dict('TblArea.json')
-        for data in area:
-            self.insert_data_to_model(model=TblArea, dict_data=data)
-        del area
-
-        art_publ_palavra_chave = self.json_to_dict('TblArtigoPublicadoPalavrasChave.json')
-        for data in art_publ_palavra_chave:
-            self.insert_data_to_model(model=TblArtigoPublicadoPalavrasChave, dict_data=data)
-        del art_publ_palavra_chave
-
-        art_publ_area_conhecimento = self.json_to_dict('TblArtigoPublicadoAreaConhecimento.json')
-        for data in art_publ_area_conhecimento:
-            self.insert_data_to_model(model=TblArtigoPublicadoAreaConhecimento, dict_data=data)
-        del art_publ_area_conhecimento
-
-        subarea = self.json_to_dict('TblSubArea.json')
-        for data in subarea:
-            self.insert_data_to_model(model=TblSubArea, dict_data=data)
-        del subarea
-
-        especialidade = self.json_to_dict('TblEspecialidade.json')
-        for data in especialidade:
-            self.insert_data_to_model(model=TblEspecialidade, dict_data=data)
-        self.stdout.write('Data upload is finished')
+        self.stdout.write(self.style.SUCCESS(f'Added data to {model.__name__} successfully'))
