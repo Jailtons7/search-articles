095b8a66f6a7919cb024e641e5107f5096cd6828
Jailton Silva <jss1@ifal.edu.br>
2022-01-19 00:11:32 -0300

    feat: crud over users

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from authentication.views import ObtainTokenPairsWithEmailView, CreateCustomUserView
+from authentication.views import (
+    ObtainTokenPairsWithEmailView,
+    CreateCustomUserView,
+    UpdateCustomUserView,
+    UserView,
+    DeleteAccountView,
+)
 
 
 urlpatterns = [
-    path('user/create/', CreateCustomUserView.as_view(), name='create_user'),
+    path('users/', UserView.as_view(), name='get_user'),
+    path('users/create/', CreateCustomUserView.as_view(), name='create_user'),
+    path('users/update/', UpdateCustomUserView.as_view(), name='update_user'),
+    path('users/delete/', DeleteAccountView.as_view(), name='update_user'),
     path('token/obtain/', ObtainTokenPairsWithEmailView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -13,6 +13,22 @@ class ObtainTokenPairsWithEmailView(TokenObtainPairView):
     serializer_class = ObtainTokenPairWithEmailSerializer
 
 
+class UserView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request):
+        user = request.user
+        user_data = {
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'cpf': user.cpf,
+            'phone': user.phone,
+            'full_name': user.get_full_name()
+        }
+        return Response(user_data, status=status.HTTP_200_OK)
+
+
 class CreateCustomUserView(APIView):
     permission_classes = (AllowAny,)
 
@@ -24,3 +40,23 @@ class CreateCustomUserView(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateCustomUserView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def put(self, request, *args, **kwargs):
+        serializer = CustomUserSerializer(instance=request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteAccountView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def delete(self, request, *args, **kwargs):
+        user = self.request.user
+        user.delete()
+        return Response({'message': 'Deleted account'}, status=status.HTTP_204_NO_CONTENT)
